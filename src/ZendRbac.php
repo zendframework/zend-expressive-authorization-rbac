f77870e501f7397ec462d08ddda8d71b7369e124
webimpress <contact@webimpress.com>
2017-09-29 10:16:11 +0100

    Added missing `@throws` tags

--- src/ZendRbac.php
+++ src/ZendRbac.php
@@ -39,6 +39,8 @@ class ZendRbac implements AuthorizationInterface
 
     /**
      * {@inheritDoc}
+     *
+     * @throws Exception\RuntimeException
      */
     public function isGranted(string $role, ServerRequestInterface $request): bool
     {
